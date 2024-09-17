02f19f939b876d1c9102172f0fb0f4a93430d9ff
aryadwiputra <aryadptr.developer@gmail.com>
2024-09-17 11:53:47 +0700

    create product migration, model and seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+            'title' => 'Dell Inspire 10',
+            'price' => 2000000,
+            'quantity' => 5,
+            'category_id' => 1,
+            'brand_id' => 1,
+            'description' => 'Occaecat cupidatat consequat eu eiusmod sint commodo aute. Fugiat commodo amet do dolore. Ullamco ullamco nisi laboris cillum ea. Excepteur exercitation laborum culpa ex do ipsum id laboris ut ad est commodo. Commodo occaecat commodo ex laborum ea esse consequat pariatur tempor qui reprehenderit. Irure ullamco esse cupidatat voluptate in sunt do officia ullamco fugiat deserunt.'
+        ]);
+    }
+}
